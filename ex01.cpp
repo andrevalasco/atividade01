a7c63b52cb7ae2fbdc945bea2641b708c71e1141
andrevalasco <88634157+andrevalasco@users.noreply.github.com>
2021-08-11 17:40:07 -0300

    Add files via upload
    Nível 01 - Exercício 01

---
+++ ex01.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main() {
+	int numero = 0;
+	
+	std::cout <<"Digite um numero: ";
+	std::cin >> numero;
+	
+	if (numero > 20 ) {
+		std::cout <<numero;
+	}
+	
+	return 0;
+}
